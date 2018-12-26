d50f837532e8068952ce710e0e1c3d41ebf4c77a
yassin zaghdoudi <yassinzaghdoudi@gmail.com>
2018-12-26 10:50:12 +0100

    src

--- src/pages/mes-produits/mes-produits.module.ts
+++ src/pages/mes-produits/mes-produits.module.ts
@@ -1,3 +1,4 @@
+import { ComponentsModule } from './../../components/components.module';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MesProduitsPage } from './mes-produits';
@@ -8,6 +9,7 @@ import { MesProduitsPage } from './mes-produits';
   ],
   imports: [
     IonicPageModule.forChild(MesProduitsPage),
+    ComponentsModule.forRoot()
   ],
 })
 export class MesProduitsPageModule {}
