949dd1fe80cd11e2d5acc2d9ebaca5a5be376281
hamzamlayeh <hamzamlayeh@outlook.com>
2018-12-26 09:18:06 +0100

    boutique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'ContratTypePage';
+  rootPage:any = 'AjouterContratPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/boutique/boutique.module.ts
+++ src/pages/boutique/boutique.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { BoutiquePage } from './boutique';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { BoutiquePage } from './boutique';
   ],
   imports: [
     IonicPageModule.forChild(BoutiquePage),
+    ComponentsModule.forRoot()
   ],
 })
 export class BoutiquePageModule {}
